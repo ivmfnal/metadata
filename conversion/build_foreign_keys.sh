d1a7532a4a72dbc1fe39132b67fe8ee7bf4db802
Igor Mandrichenko <igorvm@gmail.com>
2020-09-28 11:19:16 -0500

    .

--- conversion/build_foreign_keys.sh
+++ conversion/build_foreign_keys.sh
@@ -9,5 +9,9 @@ alter table parent_child add foreign key (child_id) references files(file_id);
 alter table files add foreign key(creator)      references users(username);
 alter table files add foreign key(namespace)    references namespaces(name);
 
+alter table files_datasets add foreign key (dataset_namespace, dataset_name) references datasets(namespace, name);
+alter table files_datasets add foreign key (file_id) references files(file_id);
+
+
 _EOF_
 
