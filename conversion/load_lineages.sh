3e913a2dc77558d50dcf4ba361dc236d7c21f3b3
Igor Mandrichenko <igorvm@gmail.com>
2020-09-30 13:15:37 -0500

    .

--- conversion/load_lineages.sh
+++ conversion/load_lineages.sh
@@ -3,6 +3,19 @@
 
 source ./config.sh
 
+$IN_DB_PSQL -q > ./data/lineages.csv << _EOF_
+
+copy (	select distinct l.file_id_source, l.file_id_dest
+		from file_lineages l, data_files f1, data_files f2
+		where f1.file_id = l.file_id_source and f1.retired_date is null 
+			and f2.file_id = l.file_id_dest and f2.retired_date is null
+) to stdout
+
+
+
+_EOF_
+
+
 $OUT_DB_PSQL << _EOF_
 
 drop table if exists parent_child;
